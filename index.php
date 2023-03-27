644ecec56f2b36d0b758a3f71644044590e709ab
flashgrinder <flashgrinder92@gmail.com>
2023-03-27 11:26:24 +0400

    change info

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
                 Приглашаем на форум
             </h2>
             <div class="invite__video gs-reveal gs-reveal--from-left">
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/oQFhze1-v_4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/YhQi_mzWONQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
     </section>
